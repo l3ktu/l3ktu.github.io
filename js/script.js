5fdb72c4cc90b1efa34fadaca2c0e3d5e73aff4e
l3ktu <l3ktuu@gmail.com>
2023-03-22 17:26:49 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ function getStats() {
     var url = "https://open.faceit.com/data/v4/players?nickname=" + username;
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
-    xhr.setRequestHeader("Authorization", "Bearer YOUR_FACEIT_API_KEY");
+    xhr.setRequestHeader("Authorization", "Bearer e91d6b16-365a-4da4-846e-d06ea670d25c");
     xhr.onreadystatechange = function () {
       if (xhr.readyState === 4 && xhr.status === 200) {
         var response = JSON.parse(xhr.responseText);
