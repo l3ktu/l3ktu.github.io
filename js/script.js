2accea4d0a75c3fb3ee0cb30b81eb09c1c545822
l3ktu <l3ktuu@gmail.com>
2023-03-23 21:29:38 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,16 +1,5 @@
-function getStats() {
-  var username = document.getElementById("username").value;
-  var url = "https://open.faceit.com/data/v4/players?nickname=" + username;
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", url);
-  xhr.setRequestHeader("Authorization", "Bearer e91d6b16-365a-4da4-846e-d06ea670d25c");
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      var response = JSON.parse(xhr.responseText);
-      var playerName = response.nickname;
-      var newUrl = `https://l3ktu.github.io/player/?username=${playerName}`;
-      window.location.href = newUrl;
-    }
-  };
-  xhr.send();
+function submitForm(event) {
+	event.preventDefault();
+	const username = document.getElementById("faceitUsername").value;
+	window.location.href = `/players/${username}`;
 }
