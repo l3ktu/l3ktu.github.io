3913904ed911215346c50f9f7c766788c21037d7
l3ktu <l3ktuu@gmail.com>
2023-03-23 21:09:56 +0200

    main

--- js/script.js
+++ js/script.js
@@ -1,22 +1,16 @@
 function getStats() {
-    var username = document.getElementById("username").value;
-    var url = "https://open.faceit.com/data/v4/players?nickname=" + username;
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.setRequestHeader("Authorization", "Bearer e91d6b16-365a-4da4-846e-d06ea670d25c");
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState === 4 && xhr.status === 200) {
-        var response = JSON.parse(xhr.responseText);
-        var level = response.games.csgo.faceit_elo / 100;
-        document.getElementById("stats").innerHTML = "Response: " + JSON.stringify(response);
-        const playerName = username;
-
-        // Construct the new URL with the player name
-        const newUrl = `https://l3ktu.github.io/player/${playerName}`;
-      
-        // Set the new URL as the current location
-        window.location.href = newUrl;
-      }
-    };
-    xhr.send();
-  }
+  var username = document.getElementById("username").value;
+  var url = "https://open.faceit.com/data/v4/players?nickname=" + username;
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.setRequestHeader("Authorization", "Bearer e91d6b16-365a-4da4-846e-d06ea670d25c");
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      var response = JSON.parse(xhr.responseText);
+      var playerName = response.nickname;
+      var newUrl = `https://l3ktu.github.io/player/${playerName}`;
+      window.location.href = newUrl;
+    }
+  };
+  xhr.send();
+}
